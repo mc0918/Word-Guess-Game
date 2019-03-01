0157eabe0caea21a46848f33330f12f3e59338a5
mc0918 <mcampbell0918@gmail.com>
2019-02-28 21:07:06 -0600

    added arrays and onclick event

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//Create all the arrays to be used in the code
+var numGuesses = 0;
+var remainingGuesses = 12;
+var lettersGuessed = [];
+var wins = 0;
+var losses = 0;
+var wordsToGuess = [];
+
+document.onkeyup = function(event) {
+  if (event.key) {
+    console.log("hi");
+  }
+};
