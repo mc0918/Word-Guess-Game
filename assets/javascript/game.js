7cedb509161987e3f578c1220908d25ae5e184a7
mc0918 <mcampbell0918@gmail.com>
2019-03-01 20:23:01 -0600

    started adding functions to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,36 @@ var remainingGuesses = 12;
 var lettersGuessed = [];
 var wins = 0;
 var losses = 0;
-var wordsToGuess = [];
+var wordsToGuess = [
+  "cave",
+  "speleothem",
+  "stalagmite",
+  "helictite",
+  "dripstone",
+  "gypsum"
+];
 
-document.onkeyup = function(event) {
-  if (event.key) {
-    console.log("hi");
-  }
-};
+//alert("press any key to begin");
+
+function guessUpdate() {
+  document.onkeyup = function(event) {
+    userInput = event.key;
+    if (lettersGuessed.includes(userInput)) {
+      console.log(userInput + " already guessed");
+    } else {
+      lettersGuessed.push(userInput);
+      console.log(lettersGuessed);
+    }
+  };
+}
+
+function gameOver() {
+  lettersGuessed.length = 0;
+}
+
+function getWord() {
+  var random = Math.floor(Math.random() * 6);
+  var picker = wordsToGuess[parseInt(random)];
+  console.log(picker);
+}
+getWord();
